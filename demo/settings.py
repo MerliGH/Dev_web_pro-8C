0c714a87836d5cb71da1f92a9ba004edd1aac623
Mariela Higuera <0322103734@ut-tijuana.edu.mx>
2025-06-29 20:17:03 -0700

    Update settimgs
    Actualizando los datos de la BD

--- demo/settings.py
+++ demo/settings.py
@@ -81,26 +81,24 @@ WSGI_APPLICATION = 'demo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-     'default': {
-         'ENGINE': 'django.db.backends.sqlite3',
-         'NAME': BASE_DIR / 'db.sqlite3',
-     }
- }
-
-
 #DATABASES = {
-#    "default": {
-#        "ENGINE": "django.db.backends.postgresql",
-#        "NAME": "pamasys_db",
-#        "USER": "pamasys_user",
-#        "PASSWORD": "PAMASYS_user12",
-#        "HOST": "127.0.0.1",
-#        "PORT": "5432",
-#    }
-#}
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "pamasys_db",
+        "USER": " ",
+        "PASSWORD": "1234",  # tu contraseña real
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
